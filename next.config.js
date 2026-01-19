5ecd83c384ecc20f69608c90649a265a027388fb
koyuki-droid <koyuki@gohalo.ai>
2026-01-19 11:05:33 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,21 @@ const nextConfig = {
         net: false,
         tls: false,
       };
+      
+      // Ignore React Native modules (not needed for web)
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@react-native-async-storage/async-storage': false,
+        'react-native': false,
+      };
+      
+      // Ignore these modules in webpack
+      const webpack = require('webpack');
+      config.plugins.push(
+        new webpack.IgnorePlugin({
+          resourceRegExp: /^@react-native-async-storage\/async-storage$/,
+        })
+      );
     }
     return config;
   },
